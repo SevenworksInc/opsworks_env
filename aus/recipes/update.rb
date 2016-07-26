840139e353788499a4d8da1d1ac3c6cd0ce58f8f
Jesther Bas <dev.jestherbas@hotmail.com>
2016-07-26 15:51:13 +0800

    update the qoute on comamnd

--- aus/recipes/update.rb
+++ aus/recipes/update.rb
@@ -5,8 +5,8 @@ end
 
 # Configure the Mailer
 execute "configure mail" do
-  command "debconf-set-selections <<< \"postfix postfix/mailname string your.hostname.com\""
-  command "debconf-set-selections <<< \"postfix postfix/main_mailer_type string 'Internet Site'\""
+  command "debconf-set-selections <<< 'postfix postfix/mailname string production.'"
+  command "debconf-set-selections <<< 'postfix postfix/main_mailer_type string \"Internet Site\"'"
   command "apt-get install -y postfix"
 end
 
