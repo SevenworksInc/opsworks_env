2f29e412206e1fbf3b4edf785076c65bb74df0d9
Jesther Bas <dev.jestherbas@hotmail.com>
2018-03-14 17:16:28 +0800

    Replacing php.ini

--- aus/recipes/inicus.rb
+++ aus/recipes/inicus.rb
@@ -1,13 +1,11 @@
-
 # Replace php.ini file.
 template '/etc/php5/apache2/php.ini' do
   source "php.ini.erb"
 end
 
-
-override['php']['directives'] = {
-  :memory_limit => '128M',
-  :post_max_size => '16M',
-  :upload_max_filesize => '12M',
-  :date.timezone => 'Asia/Manila'
-}
+#override['php']['directives'] = {
+#  :memory_limit => '128M',
+#  :post_max_size => '16M',
+#  :upload_max_filesize => '12M',
+#  :date.timezone => 'Asia/Manila'
+#}
