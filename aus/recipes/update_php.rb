0ef08811c4df51552503271beaf733ffde1c06ab
Jesther Bas <dev.jestherbas@hotmail.com>
2018-05-02 12:53:14 +0800

    Add dependencies installation yes

--- aus/recipes/update_php.rb
+++ aus/recipes/update_php.rb
@@ -2,7 +2,7 @@
 execute "apt-get-update-php" do
   command "add-apt-repository ppa:ondrej/php5-5.6"
   command "apt-get update"
-  command "apt-get install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml"
+  command "apt-get install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml -y"
   command "a2dismod php5"
   command "a2enmod php5.6"
   command "service apache2 restart"
